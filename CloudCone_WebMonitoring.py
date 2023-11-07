ee0a5bd33cb7563526812de41ff592b69371c2cf
fungj <feng9732@qq.com>
2023-11-07 10:47:31 +0800

    CloudCone VPS库存监控的脚本

--- CloudCone_WebMonitoring.py
+++ CloudCone_WebMonitoring.py
@@ -14,7 +14,7 @@ url = 'https://app.cloudcone.com/vps/205/create?token=pre-bf-vps-23-2'
 DEFAULT_HEADERS = {
     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36',
-    'Referer': 'https://app.cloudcone.com/vps/205/create?token=pre-bf-vps-23-2',
+    'Referer': '{url}',
 }
 
 
--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 # Qcloud_TrafficPackages
 腾讯云轻量云流量监控，超标自己关机
+
+# CloudCone_WebMonitoring
+CloudCone家的库存监控，有货自己发送邮件提醒。  
+通过修修改URL变量来控制要监控的库存地址。  
+邮箱信息修改第69行起的相关参数即可。
